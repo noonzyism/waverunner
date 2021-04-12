34db95bbe6ae091f741ac06ee048ced8cfcb60fe
Steven Nunes <snunes012@gmail.com>
2021-04-12 11:29:44 -0400

    volume alerting

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from threading import Thread
 
-import websockets, json, numpy
+import websockets, json, numpy, statistics
 import config
 from binance.client import Client
 from binance.enums import *
@@ -13,21 +13,30 @@ pairs = [
     "BTCUSDT",
     "ETHUSDT",
     "ADAUSDT",
+    "UNIUSDT",
     "STORJUSDT",
     "VETUSDT",
     "ONEUSDT",
     "ONTUSDT",
-    "UNIUSDT",
     "DOGEUSDT",
     "ZENUSDT",
     "OXTUSDT",
     "QTUMUSDT",
     "SOLUSDT",
-    "BANDUSDT",
+    "HBARUSDT",
     "NEOUSDT",
     "XLMUSDT",
     "ATOMUSDT",
-    "ZRXUSDT"
+    "ZRXUSDT",
+    "MANAUSDT",
+    "ENJUSDT",
+    "NANOUSDT",
+    "ZILUSDT",
+    "MATICUSDT",
+    "ALGOUSDT",
+    "IOTAUSDT",
+    "RVNUSDT",
+    "LINKUSDT"
 ]
 
 streams = map(lambda s: s.lower() + "@kline_1m", pairs)
@@ -38,7 +47,10 @@ SOCKET = "wss://stream.binance.com:9443/ws/" + endpoint
 opens = { p : [] for p in pairs }
 closes = { p : [] for p in pairs }
 rates = { p : [] for p in pairs }
+volumes = { p : [] for p in pairs }
+trades = { p : [] for p in pairs }
 prices = { p : 0 for p in pairs }
+messages = { p : "none yet" for p in pairs }
 
 last_msg = "none yet"
 channel = -1 # should start with a default channel but I'm lazy
@@ -71,16 +83,30 @@ async def status():
 
 async def check_for_alerts(pair):
     global opens, closes, rates
-    last_2_rates = rates[pair][-2:]
-    s = sum(last_2_rates)
+    last_3_rates = rates[pair][-3:]
+    prior_30_vols = volumes[pair][-31:]
+    last_vol = prior_30_vols.pop()
+    prior_30_trades = trades[pair][-31:]
+    last_trade = prior_30_trades.pop()
+    s = sum(last_3_rates)
     if (s > 0.012):
-        alert = ":fire: SURGE ALERT: {} (${}) over last 2m is up {}%".format(pair[:-4], round(prices[pair], 3), round(s*100, 3))
+        alert = ":fire: SURGE ALERT: {} (${}) over last 3m is up {}%".format(pair[:-4], round(prices[pair], 3), round(s*100, 3))
         if (channel != -1):
             await channel.send(alert)
     if (s < -0.02):
-        alert = ":rotating_light: CRASH ALERT: {} (${}) over last 2m is down {}%".format(pair[:-4], round(prices[pair], 3), round(s*100, 3))
+        alert = ":rotating_light: CRASH ALERT: {} (${}) over last 3m is down {}%".format(pair[:-4], round(prices[pair], 3), round(s*100, 3))
         if (channel != -1):
             await channel.send(alert)
+    # if (len(prior_30_vols) > 1 and last_vol > 6.0*statistics.fmean(prior_30_vols)):
+    #     mean = statistics.fmean(prior_30_vols)
+    #     alert = ":chart_with_upwards_trend: VOLUME ALERT: {} asset volume has spiked {}x. Past minute: {}, Previous 30 minutes: {}".format(pair[:-4], round(last_vol/mean, 2), last_vol, round(mean, 3))
+    #     if (channel != -1):
+    #         await channel.send(alert)
+    # if (len(prior_30_trades) > 1 and last_trade > 5.0*statistics.fmean(prior_30_trades)):
+    #     mean = statistics.fmean(prior_30_trades)
+    #     alert = ":money_with_wings: TRADE ALERT: {} trade volume has spiked {}x. Past minute: {}, Previous 30 minutes: {}".format(pair[:-4], round(last_trade/mean, 2), last_trade, round(mean, 3))
+    #     if (channel != -1):
+    #         await channel.send(alert)
 
 async def output_prices():
     global prices
@@ -113,8 +139,14 @@ async def on_message(message):
                         await status()
                     elif command == "prices":
                         await output_prices()
-                    elif command == "last":
-                        await channel.send("Last message was: {}".format(last_msg))
+                    elif command.startswith("last"):
+                        second_arg = command.replace('last','').strip()
+                        if (second_arg == ''):
+                            await channel.send("Last message was: {}".format(last_msg))
+                        elif (second_arg in messages):
+                            await channel.send("Last message for {} was: {}".format(second_arg, messages[second_arg]))
+                        else:
+                            await channel.send("Come again?")
                 except Exception as ex:
                     print(ex)
                     
@@ -125,22 +157,27 @@ async def init_connection():
     websocket = await websockets.connect(SOCKET)
 
 async def listener():
-    global opens, closes, rates, prices, last_msg
+    global opens, closes, rates, prices, volumes, trades, messages, last_msg
     async for message in websocket:
         last_msg = message
         json_message = json.loads(message)
         candle = json_message['k']
         pair = candle['s']
         is_candle_closed = candle['x']
+        messages[pair] = message
         prices[pair] = float(candle['c'])
         if is_candle_closed:
             print(message)
             open_price = float(candle['o'])
             close_price = float(candle['c'])
             rate = (close_price/open_price) - 1.0
+            volume = float(candle['v'])
+            trade = float(candle['n'])
             opens[pair].append(open_price)
             closes[pair].append(close_price)
             rates[pair].append(rate)
+            volumes[pair].append(volume)
+            trades[pair].append(trade)
             await check_for_alerts(pair)
 
 if __name__ == '__main__':
